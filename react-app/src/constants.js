02de39fb8fa38b421cd62927ca0d2e180cacd497
AbihaFatima <abiha.fatima1908@gmail.com>
2023-04-26 19:52:26 +0400

    :rewind: rolling back to localhost

--- react-app/src/constants.js
+++ react-app/src/constants.js
@@ -1,6 +1,3 @@
 // Consist of Backend API Links
 
-// export const APIBaseURL = "http://localhost:8080"
-
-//deployed version
-export const APIBaseURL = "https://remote-workspace-server.onrender.com" 
+export const APIBaseURL = "http://localhost:8080"
